2b843ba6ce8bf2fbda76e9baa913c88f681de4e4
amidou.zabre <amidou.zabre@gmail.com>
2025-03-06 22:17:09 +0000

    hook personnalisé 1

--- src/hooks/useIncrement.jsx
+++ src/hooks/useIncrement.jsx
@@ -2,7 +2,10 @@ import {useState} from "react";
 
 /**
  *
- * @param initialValue
+ * @param {number} base
+ * @param {number} max
+ * @param {number} min
+ * @param {number} step
  * @returns {{count: number, increment: (function(): void), decrement: (function(): void)}}
  */
 export function  useIncrement ({base = 0, max = Infinity, min = -Infinity, step = 1}) {
