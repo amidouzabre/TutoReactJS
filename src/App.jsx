e45a042683beb37f7910a11173b96d217daa51ae
amidou.zabre <amidou.zabre@gmail.com>
2025-03-06 15:37:11 +0000

    hook personnalisé

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { useRef } from "react"
-import { Input } from "./components/forms/input"
+import {useToggle} from "./hooks/useToggle.jsx";
+import {useIncrement} from "./hooks/useIncrement.jsx";
 
 
 function App() {
 
-  const ref = useRef()
-  console.log("App", ref)
- 
+  const [checked, toggleCheck] = useToggle()
+  const {count , increment, decrement} = useIncrement({
+    base: 0,
+    max: 10,
+    min: 0,
+    step: 1
+  })
+
 
   return <div>
-      <Input ref={ref} label="prefix" />
+    <div>
+      <input type="checkbox" checked={checked} onChange={toggleCheck} />
+      {checked && "Je suis coché"}
+    </div>
+
+    <div>
+      Compteur : {count}
+      <button onClick={increment}>Incrementer</button>
+      <button onClick={decrement}>Decrementer</button>
+    </div>
 
   </div>
+
 }
 
 export default App
---
+++ src/hooks/useIncrement.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+/**
+ *
+ * @param initialValue
+ * @returns {{count: number, increment: (function(): void), decrement: (function(): void)}}
+ */
+export function  useIncrement ({base = 0, max = Infinity, min = -Infinity, step = 1}) {
+    const [state, setState] = useState(base)
+
+    return {
+        count: state,
+        increment: () => setState(v => v < max ? v+step : v),
+        decrement: () => setState(v => v > min ? v-step : v)
+    }
+
+}
